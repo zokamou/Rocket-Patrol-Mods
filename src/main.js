86396d34c7ef3ed0df23ae35f9f817b89cfa4695
zfeller <zfeller@ucsc.edu>
2023-10-18 22:54:14 -0700

    checkpoint

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+let config = {
+    type: Phaser.CANVAS, 
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+let game = new Phaser.Game(config);
+
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
