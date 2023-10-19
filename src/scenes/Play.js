8ebab7e90a1b8f003c1b7c142d861314f324934f
zfeller <zfeller@ucsc.edu>
2023-10-18 23:12:07 -0700

    goodnight

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -24,6 +24,11 @@ class Play extends Phaser.Scene{
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -33,6 +38,9 @@ class Play extends Phaser.Scene{
 
     update() {
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update;
+        this.p1Rocket.update();
+        this.ship01.update();               // update spaceships (x3)
+        this.ship02.update();
+        this.ship03.update();
     }
 }
